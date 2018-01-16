116778d3880fced9e22db7ccb9efb3a30a77c586
heather <crystal_tree@hotmail.com>
2018-01-16 14:13:04 -0600

    Added counter for number of guesses until correct.

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,12 +3,18 @@ import random
 correct = 'you guessed correctly!'
 too_low = 'too low'
 too_high = 'too high'
-
+total_guesses = 0
 
 def configure_range():
     '''Set the high and low values for the random number'''
     return 1, 10
 
+def increment_guesses():
+    global total_guesses
+    total_guesses=total_guesses+1
+
+def display_guesses():
+    print("You got it in ",total_guesses," guesses.")
 
 def generate_secret(low, high):
     '''Generate a secret number for the user to guess'''
@@ -17,12 +23,14 @@ def generate_secret(low, high):
 
 def get_guess():
     '''get user's guess'''
+    increment_guesses()
     return int(input('Guess the secret number? '))
 
 
 def check_guess(guess, secret):
     '''compare guess and secret, return string describing result of comparison'''
     if guess == secret:
+        display_guesses()
         return correct
     if guess < secret:
         return too_low
